b8eaf4513e8b33ab918d28273d77d38042645d93
Atiye <atiyez@yahoo.com>
2024-12-15 20:23:57 +0330

    Add project files.

---
+++ Start-Again/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Start_Again.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
